f465305dd267a386b30b96ac5b9d17da723f595b
Potter <75838259@qq.com>
2025-02-12 20:39:28 +0800

    redux的基本使用

---
+++ redux/01-base/src/main.jsx
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+
+const counter = document.getElementById("counter");
+const addButton = document.getElementById("add-button");
+const minusButton = document.getElementById("minus-button");
+
+const State = {
+  ADD: "ADD",
+  MINUS: "MINUS",
+};
+
+const initState = {
+  number: 0,
+};
+
+function reducer(state = initState, action) {
+  const { type } = action;
+  switch (type) {
+    case State.ADD:
+      return { number: state.number + 1 };
+    case State.MINUS:
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.subscribe(render);
+
+function render() {
+  counter.innerHTML = store.getState().number;
+}
+
+addButton.addEventListener("click", () => {
+  store.dispatch({ type: State.ADD });
+});
+
+minusButton.addEventListener("click", () => {
+  store.dispatch({ type: State.MINUS });
+});
+
+render();
